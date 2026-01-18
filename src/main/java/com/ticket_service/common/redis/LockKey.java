9b5ea42db27e706bb9f0ead1cfded7b4d2b077c2
hingjae <ljh23270@naver.com>
2026-01-18 21:33:19 +0900

    LockKey 용도 설명 주석 추가.

--- src/main/java/com/ticket_service/common/redis/LockKey.java
+++ src/main/java/com/ticket_service/common/redis/LockKey.java
@@ -1,7 +1,9 @@
 package com.ticket_service.common.redis;
 
 public class LockKey {
+    // DB TicketStock 테이블 접근 시 사용하는 분산락
     private static final String TICKET_STOCK_LOCK_PREFIX = "LOCK:TICKET_STOCK:";
+    // 대기열 사용자 중복 진입 방지용 락
     private static final String QUEUE_USER_LOCK_PREFIX = "LOCK:QUEUE:USER:";
 
     public static String ticketStock(Long id) {
