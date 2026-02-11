25de1b3fae5df4b35ca2211251db169d4729d042
hingjae <ljh23270@naver.com>
2026-02-12 00:16:58 +0900

    샘플데이터 추가.

--- src/main/resources/sql/performance-data.sql
+++ src/main/resources/sql/performance-data.sql
@@ -1,1 +1,3 @@
-INSERT INTO ticket.ticket_stock (remaining_quantity, total_quantity, concert_id, version) VALUES (5000, 5000, null, 0)
+INSERT INTO ticket.concert (title, open_at, close_at) VALUES ('2025 Performance Test Concert', '2025-01-01 10:00:00', '2025-12-31 23:59:59');
+
+INSERT INTO ticket.ticket_stock (remaining_quantity, total_quantity, concert_id, version) VALUES (5000, 5000, 1, 0)
